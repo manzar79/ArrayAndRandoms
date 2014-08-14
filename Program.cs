f128e6ed96f903d3e1847813129883679e8edd0a
manzar79 <manzar79@gmail.com>
2014-08-14 11:15:54 -0400

    ArrayAndRandoms

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimArray
+{
+
+   class Program
+   {
+       static void Main(string[] args)
+       {
+
+           int[,] dice = new int[4, 2];                   
+                                                             
+           Random myRandom = new Random();
+
+           for (int i = 0; i < 4; i++)          
+           {
+               for (int j = 0; j < 2; j++)             
+               {
+                   dice[i, j] = myRandom.Next(6) + 1;
+
+               }
+           }
+           for (int i = 0; i < 4; i++)         
+           {
+               for (int j = 0; j < 2; j++)            
+               {
+                   Console.Write(dice[i, j] + "  ");
+
+               }
+               Console.WriteLine();
+           }
+           Console.ReadLine();
+
+       }
+   }
+}
+
---
+++ SimpleArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+
+           int[] dice = new int[6];    // the allowable index numbers are therefore 1 to 5
+           Random myRandom = new Random();
+
+           for (int i = 1; i < 6; i++) // the allowable index numbers are 1 to 5
+           {
+               dice[i] = myRandom.Next(6) + 1;
+           }
+
+           for (int i = 1; i < 6; i++) // the allowable index numbers are 1 to 5
+           {
+               Console.WriteLine(dice[i]);
+           }
+           Console.ReadLine();
+
+       }
+   }
+}
+        
